27ca9b50cb7f8ff31c215a4496dbd113b4cbb94d
sebifp <sebiflorinp@gmail.com>
2024-02-26 13:48:49 +0200

    FIX locations urls and update documentation

--- backend/location/urls.py
+++ backend/location/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CreateLocations, UpdateDeleteLocations
 
 urlpatterns = [
-    path('employer/<int:employer_id>/locations/', CreateLocations.as_view(), name='create-locations'),
-    path('employer/<int:employer_id>/locations/<int:location_id>', UpdateDeleteLocations.as_view(),
+    path('employers/<int:employer_id>/locations/', CreateLocations.as_view(), name='create-locations'),
+    path('employers/<int:employer_id>/locations/<int:location_id>', UpdateDeleteLocations.as_view(),
          name='update-delete-locations')
 ]
